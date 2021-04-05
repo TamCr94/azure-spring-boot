cd1717125e36c4dd557bcc86238a36c7508b089f
tamdo <tamcr94.dev@gmail.com>
2021-04-06 06:55:54 +0700

    update maven module

---
+++ active-directory/src/test/java/com/example/activedirectory/ActiveDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activedirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
